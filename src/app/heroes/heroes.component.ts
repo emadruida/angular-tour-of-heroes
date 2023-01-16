a673c463a7945224f7ba758827dc3dfccb7b903a
Ernesto de Miguel <emadruida@gmail.com>
2023-01-16 21:02:09 +0100

    Get data from a server

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,9 +10,7 @@ import { Hero } from './../hero';
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
-  constructor(
-    private heroService: HeroService
-  ) {}
+  constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
     this.getHeroes();
@@ -20,5 +18,21 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService.addHero({ name } as Hero).subscribe((hero) => {
+      this.heroes.push(hero);
+    });
+  }
+
+  delete(hero: Hero): void {
+    this.heroService.deleteHero(hero.id).subscribe(
+      _ => this.heroes = this.heroes.filter(h => h !== hero)
+    );
   }
 }
