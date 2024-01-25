55a3d2589f0f5134be7a4915793f25675f5b2d32
Ernesto de Miguel <emadruida@gmail.com>
2024-01-25 09:33:06 +0100

    Standalone app

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,14 +1,32 @@
-import { Location } from '@angular/common';
+import { Location, UpperCasePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { FormsModule } from '@angular/forms';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
-  templateUrl: './hero-detail.component.html',
+  template: `
+    @if (hero) {
+      <div>
+        <h2>{{ hero.name | uppercase }} Details</h2>
+        <div>
+          <label for="name">Hero name: </label>
+          <input id="name" [(ngModel)]="hero.name" placeholder="name" />
+        </div>
+        <button type="button" (click)="goBack()">go back</button>
+        <button type="button" (click)="save()">save</button>
+      </div>
+    }
+  `,
   styleUrls: ['./hero-detail.component.css'],
+  standalone: true,
+  imports: [
+    FormsModule,
+    UpperCasePipe,
+  ],
 })
 export class HeroDetailComponent implements OnInit {
   hero?: Hero;
@@ -17,7 +35,7 @@ export class HeroDetailComponent implements OnInit {
     private route: ActivatedRoute,
     private heroService: HeroService,
     private location: Location
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.getHero();
--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -3,9 +3,21 @@ import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-messages',
-  templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+  template: `
+    @if (messageService.messages.length) {
+      <div>
+        <h2>Messages</h2>
+        <button type="button" class="clear" (click)="messageService.clear()">Clear messages</button>
+        @for (message of messageService.messages; track $index) {
+          <div>{{ message }}</div>
+        }
+      </div>
+    }
+  `,
+  styleUrls: ['./messages.component.css'],
+  standalone: true,
+  imports: []
 })
 export class MessagesComponent {
-  constructor(public messageService: MessageService) {}
+  constructor(public messageService: MessageService) { }
 }
